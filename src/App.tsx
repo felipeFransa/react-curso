fd037d5965d143a41358452377adc5930e676ae6
felipe <antoniofelipesilva@live.com>
2022-12-26 17:41:13 -0300

    version.1.0.7

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,14 @@ import { Header } from './components/Header'
 import { Main } from './components/Main'
 import { Formulario } from './components/Formulario'
 import { Patrulha } from './components/Patrulha';
-import { useEffect, useState } from 'react';
-import { Pots } from './types/Pots'
+import { PotsPage } from './components/LoadPots'
 
 
 const App = () => {
- const [pots, setPots] = useState<Pots[]>([]);
- const [loading, setLoading] = useState<boolean>(false);
-
- useEffect(() =>{
-  loadPots();
- }, [])
-
- const loadPots = async () => {
-  try {
-    setLoading(true)
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    let json = await response.json();
-    setLoading(false)
-    setPots(json);
-  }
-  catch(e) {
-    setLoading(false);
-    alert('Erro! Tente mais tarde')
-  }
- }
-
-
   return (
     <div>
-      {loading && <div>Carregando....</div>}
-      <div>
-      <div>Total de pots {pots.length}</div>
-      {pots.map((item, index) => (
-        <div key={index}>
-          <h4>{item.title}</h4><br />
-          <p>{item.body}</p>
-        </div>
-      ))}
-      </div>
+      <PotsPage/>
     </div>
-  )
+  ) 
 }
 export default App;
---
+++ src/components/LoadPots.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { Pots } from '../types/Pots'
+
+
+export const PotsPage = () => {
+  const [pots, setPots] = useState<Pots[]>([]);
+ const [loading, setLoading] = useState<boolean>(false);
+
+ useEffect(() =>{
+  loadPots();
+ }, [])
+
+ const loadPots = async () => {
+  try {
+    setLoading(true)
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let json = await response.json();
+    setLoading(false)
+    setPots(json);
+  }
+  catch(e) {
+    setLoading(false);
+    alert('Erro! Tente mais tarde')
+  }
+ }
+ return (
+  <div>
+    {loading && <div>Carregando....</div>}
+    {!loading && pots.length > 0 && 
+    <>
+      <div>
+        <div>Total de pots {pots.length}</div>
+        {pots.map((item, index) => (
+        <div key={index}>
+        <h4>{item.title}</h4><br />
+        <p>{item.body}</p>
+        </div>
+    ))}
+      </div>
+    </> }
+  </div>
+)
+}
