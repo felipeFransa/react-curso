19c7077ba56df1eb240752d29fc2264cb4df7c02
felipe <antoniofelipesilva@live.com>
2022-12-27 18:50:42 -0300

    version.1.1.3

--- src/api.ts
+++ src/api.ts
@@ -1,17 +1,29 @@
-const BASE = 'https://jsonplaceholder.typicode.com'
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com'
+})
 
 export const api = {
   getAllPosts: async () => {
+    let response = await http.get('/posts')
+    return response.data;
+    /*
     let response = await fetch(`${BASE}/posts`);
     let json = await response.json();
     return json;
+    */
   },
   addNewPost: async (title: string, body: string, userId: number) => {
+    let response = await http.post('/posts', {title, body, userId})
+    return response.data;
+    /*
     let response = await fetch(`${BASE}/posts`, {
       method: 'POST',
       body: JSON.stringify({title, body, userId: 1}),
       headers:{'Content-Type': 'application/json'}});
     let json = await response.json();
     return json;
+    */
   }
 }
