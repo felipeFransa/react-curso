bf8cf834599f16bb65d5019507ec227c9dfbbbe4
felipe <antoniofelipesilva@live.com>
2022-12-28 18:48:14 -0300

    version.1.2.1

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,22 @@
+import { useSearchParams } from 'react-router-dom'
+
 export const About = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const setOrder = (order: 'asc' | 'desc') => {
+    searchParams.set('order', order);
+    setSearchParams(searchParams)
+  }
+
   return(
-    <div>Pagina About:
+    <div>
+      Filtro: {searchParams.get('filter')} <br />
+      Ordem: {searchParams.get('order')}
+      <hr />
+      <button onClick={()=>setOrder('asc')}>Asc</button>
+      <button onClick={()=>setOrder('desc')}>Desc</button>
+      <hr />
+      Pagina About:
       <ul>
         <li>Felipe</li>
         <li>Silva</li>
