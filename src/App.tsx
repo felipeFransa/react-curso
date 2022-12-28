8599e80cf0b8ab6e1a9ccaa94ff747dd30ba4715
felipe <antoniofelipesilva@live.com>
2022-12-28 16:27:57 -0300

    version.1.1.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { Routes, Route} from 'react-router-dom'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
 
 const App = () => {
   
   return (
-    <div>
+    <div className="p-4">
+      <header>
+        <h1>Titulo do site</h1>
+      </header>
+      <hr />
+      <div className="py-4">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/sobre" element={<About />}/>
+        </Routes>
+      </div>
+      <hr />
+      <footer>todos ps direitos reservados</footer>
     </div>
   ) 
 }
